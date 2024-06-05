c7ca09d62de1c79ad59dcfb409b945c3b1558727
otavio lemes <otavio11lemes@outlook.com>
2024-06-05 19:09:56 -0300

    adicionando bootstrap

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { authenticateToken } = require('../middlewares/authMiddleware');
 
-router.post('/users', userController.createUser);
-
-// Outras rotas para produtos, ordens, etc...
+router.post('/login', userController.authenticateUser);
+router.get('/protectedRoute', authenticateToken, (req, res) => {
+  res.send('This route is protected');
+});
 
 module.exports = router;
